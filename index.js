00d6b43e1e817393d410c3dbd2f40a09487827d2
Savely <savely.jadan@gmail.com>
2024-06-14 15:13:17 +0700

    Created a middleware checking user authorization. Created a GET handler for getting user's data

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { validationResult } from 'express-validator'
 
 import { registerValidation } from './validations/auth.js'
 import UserModel from './models/User.js'
+import checkAuth from './utils/checkAuth.js'
 
 dotenv.config()
 
@@ -109,6 +110,28 @@ app.post('/auth/register', registerValidation, async (req, res) => {
         console.log(err)
         res.status(500).json({
             message: 'Не удалось зарегестрироваться',
+        })
+    }
+})
+
+app.get('/auth/me', checkAuth, async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.userId)
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'Пользователь не найден',
+            })
+        }
+
+        const { passwordHash, ...userData } = user._doc
+
+        res.json({
+            ...userData,
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: 'Нет доступа',
         })
     }
 })
