c9e33009b2afa9f2792a7a48a1e0cb59f92658a1
Savely <savely.jadan@gmail.com>
2024-06-15 21:06:01 +0700

    Added file upload functionality with multer library. Created a middleware handleValidationErrors.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-
+import multer from 'multer'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
@@ -7,6 +7,7 @@ import checkAuth from './utils/checkAuth.js'
 import * as Validation from './validations.js'
 import * as UserController from './controllers/UserController.js'
 import * as PostController from './controllers/PostController.js'
+import handleValidationErrors from './utils/handleValidationErrors.js'
 
 dotenv.config()
 
@@ -17,35 +18,61 @@ mongoose
 
 const app = express()
 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname)
+    },
+})
+
+const upload = multer({ storage })
+
 app.use(express.json())
+app.use('/uploads', express.static('uploads'))
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    })
+})
 
 app.get('/', (req, res) => {
     res.send(' 111 Hello world')
 })
 
-app.post('/auth/login', Validation.loginValidation, UserController.login)
-
+app.post(
+    '/auth/login',
+    Validation.loginValidation,
+    handleValidationErrors,
+    UserController.login
+)
 app.post(
     '/auth/register',
     Validation.registerValidation,
+    handleValidationErrors,
     UserController.register
 )
-
 app.get('/auth/me', checkAuth, UserController.getMe)
 
 app.get('/posts', PostController.getAll)
-
 app.get('/posts/:id', PostController.getOne)
-
 app.post(
     '/posts',
     checkAuth,
     Validation.postCreateValidation,
+    handleValidationErrors,
     PostController.create
 )
-
 app.delete('/posts/:id', checkAuth, PostController.remove)
-app.patch('/posts/:id', checkAuth, PostController.update)
+app.patch(
+    '/posts/:id',
+    checkAuth,
+    Validation.postCreateValidation,
+    handleValidationErrors,
+    PostController.update
+)
 
 app.listen(4444, (err) => {
     if (err) {
