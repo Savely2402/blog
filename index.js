e2ad24650ede3c595cc4b100318fbba0c8a5fcb1
Savely <savely.jadan@gmail.com>
2024-06-15 19:44:43 +0700

    Created controller for managing posts
    - Added  method to fetch all posts with user population.
    - Added  method to increment view count and return a post.
    - Added  method to delete a post.
    - Added  method to create a new post.
    - Added  method to update an existing post.

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
+import checkAuth from './utils/checkAuth.js'
 import * as Validation from './validations.js'
 import * as UserController from './controllers/UserController.js'
-import checkAuth from './utils/checkAuth.js'
+import * as PostController from './controllers/PostController.js'
 
 dotenv.config()
 
@@ -31,6 +32,20 @@ app.post(
 )
 
 app.get('/auth/me', checkAuth, UserController.getMe)
+
+app.get('/posts', PostController.getAll)
+
+app.get('/posts/:id', PostController.getOne)
+
+app.post(
+    '/posts',
+    checkAuth,
+    Validation.postCreateValidation,
+    PostController.create
+)
+
+app.delete('/posts/:id', checkAuth, PostController.remove)
+app.patch('/posts/:id', checkAuth, PostController.update)
 
 app.listen(4444, (err) => {
     if (err) {
