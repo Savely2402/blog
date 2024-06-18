e56bde5fffea6c843330ad390997084884d94f59
Savely <savely.jadan@gmail.com>
2024-06-19 00:50:05 +0700

    Initialized redux store, create first slice - posts.js.  Fetch posts from backend using redux. Add CORS to backend to make requests from frontend.

--- blog-backend/index.js
+++ blog-backend/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import multer from 'multer'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 import { handleValidationErrors, checkAuth } from './utils/index.js'
 import * as Validation from './validations.js'
@@ -28,6 +29,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 app.use(express.json())
+app.use(cors())
 app.use('/uploads', express.static('uploads'))
 
 app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
